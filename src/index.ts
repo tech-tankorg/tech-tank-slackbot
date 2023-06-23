6cb5d7afee874827b50ecab9b0d7cbe693fdac35
Nonso <nonsootoh@gmail.com>
2023-06-22 22:39:53 -0400

    chore: testing in prod

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // import app from "@utils/config/slack-config.ts";
 import app from "../utils/config/slack-config.ts";
 import { greet_new_team_member } from "./Events/greetings.ts";
-// import { sayHello } from "./Events/sayHello.ts";
+import { sayHello } from "./Events/sayHello.ts";
 
 // This command is for testing purposes. Leave here in case we want to test in the future.
-// sayHello();
+sayHello();
 
 greet_new_team_member();
 
