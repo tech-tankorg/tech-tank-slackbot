7327acedfc306460044fb8661766b56c1569cee9
Nonso <nonsootoh@gmail.com>
2023-06-22 22:34:06 -0400

    chore: removes testing hello function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // import app from "@utils/config/slack-config.ts";
 import app from "../utils/config/slack-config.ts";
 import { greet_new_team_member } from "./Events/greetings.ts";
-import { sayHello } from "./Events/sayHello.ts";
+// import { sayHello } from "./Events/sayHello.ts";
+
+// This command is for testing purposes. Leave here in case we want to test in the future.
+// sayHello();
 
 greet_new_team_member();
-sayHello();
 
 (async () => {
   // Start your app
