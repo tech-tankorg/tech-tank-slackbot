8bcdc6d3d82c22e3c4df27e1395702b08fcc2a9d
Nonso <nonsootoh@gmail.com>
2023-07-01 19:42:38 -0400

    refactor: puts non private vars into channelConfig

--- utils/config/channel-config.ts
+++ utils/config/channel-config.ts
@@ -1,19 +1,19 @@
 export const admins = {
-  chris: process.env.ADMIN_CHANNEL_ID_CHRIS ?? "",
-  sammy: process.env.ADMIN_CHANNEL_ID_SAMMY ?? "",
-  eric: process.env.ADMIN_CHANNEL_ID_ERIC ?? "",
-  johann: process.env.ADMIN_CHANNEL_ID_JOHANN ?? "",
-  nonso: process.env.ADMIN_CHANNEL_ID_NONSO ?? "",
+  chris: "U04DDM4LQAD",
+  sammy: "U04SKSY6K1C",
+  eric: "U053Y4S16TE",
+  johann: "U04D05DDPJ6",
+  nonso: "U0586TY9NP2",
 } as const;
 
 export const channels = {
-  intro: process.env.MAIN_CHANNEL_ID_INTRO ?? "",
-  networking: process.env.MAIN_CHANNEL_ID_NETWORKING ?? "",
-  social: process.env.MAIN_CHANNEL_ID_SOCIAL ?? "",
-  study: process.env.MAIN_CHANNEL_ID_STUDY ?? "",
-  jobs: process.env.MAIN_CHANNEL_ID_JOBS ?? "",
-  random: process.env.MAIN_CHANNEL_ID_RANDOM ?? "",
-  ugh: process.env.MAIN_CHANNEL_ID_UGH ?? "",
-  resources: process.env.MAIN_CHANNEL_ID_RESOURCES ?? "",
-  project_proposals: process.env.MAIN_CHANNEL_ID_PROJECT_PROPOSAL ?? "",
+  intro: "C052KD3ELTH",
+  networking: "C057LAEN8DP",
+  social: "C04UA2H8ARG",
+  study: "C04S75JBKPA",
+  jobs: "C04J7CPHVMW",
+  random: "C04CTH78SEA",
+  ugh: "C052W5AU9BM",
+  resources: "C04U5MWEXP1",
+  project_proposals: "C05DVA1HX9U",
 } as const;
