44135ffe359b08fd58a596473a7b93fbfeac938c
Nonso <nonsootoh@gmail.com>
2023-07-01 21:38:21 -0400

    fix: checks if message is already scheduled

--- src/Events/send-scheduled-message.ts
+++ src/Events/send-scheduled-message.ts
@@ -4,6 +4,8 @@ import { generate_scheduled_messages } from "../../utils/helpers/generate-schedu
 import Axiom from "../../utils/config/axiom-config.ts";
 import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
 
+import { flatten_all_scheduled_messages_reponse } from "../../utils/helpers/flatten-object.ts";
+
 export const send_scheduled_message = async (
   lst_msgs: string[],
   channel: string,
@@ -16,8 +18,21 @@ export const send_scheduled_message = async (
   );
 
   try {
+    const all_scheduled_messages =
+      await app.client.chat.scheduledMessages.list();
+
+    const scheduled_message_array = all_scheduled_messages.scheduled_messages
+      ? all_scheduled_messages.scheduled_messages
+      : [];
+
+    const flattened_all_schedule_messages =
+      flatten_all_scheduled_messages_reponse(scheduled_message_array);
+
     const scheduled_messages = await Promise.all(
-      message.map(async (item) => app.client.chat.scheduleMessage(item))
+      message.map(async (item) => {
+        if (!flattened_all_schedule_messages.includes(item.text))
+          return app.client.chat.scheduleMessage(item);
+      })
     );
 
     await Axiom.ingestEvents(AXIOM_DATA_SET, [{ scheduled_messages }]);
--- utils/helpers/flatten-object.ts
+++ utils/helpers/flatten-object.ts
@@ -1,3 +1,4 @@
+import type { ScheduledMessage } from "../types/projectTypes.d.ts";
 export const flatten_object = (obj: Record<string, string[]>) => {
   const values = Object.values(obj);
 
@@ -5,6 +6,18 @@ export const flatten_object = (obj: Record<string, string[]>) => {
 
   values.forEach((value) => {
     new_array.push(...value);
+  });
+
+  return new_array;
+};
+
+export const flatten_all_scheduled_messages_reponse = (
+  obj: ScheduledMessage[]
+) => {
+  const new_array: Array<string | undefined> = [];
+
+  obj.forEach((value) => {
+    new_array.push(value.text);
   });
 
   return new_array;
