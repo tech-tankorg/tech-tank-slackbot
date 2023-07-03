9276de42221c62886950886ba84b6992299b562d
Nonso <nonsootoh@gmail.com>
2023-07-03 16:32:28 -0400

    chore: changes flag

--- utils/constants/consts.ts
+++ utils/constants/consts.ts
@@ -1,4 +1,4 @@
-export const AXIOM_DATA_SET = false ? "slack-bot" : "slack-bot-dev";
+export const AXIOM_DATA_SET = true ? "slack-bot" : "slack-bot-dev";
 
 export const GENERAL_QUESTIONS_START_DATE = "June 22, 2023 16:00:00 UTC";
 
