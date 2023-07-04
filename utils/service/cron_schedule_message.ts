a88bc74fd72ee36f7908251d75c342a811397148
Nonso <nonsootoh@gmail.com>
2023-07-03 21:09:12 -0400

    fix: issue with cron job running out of sync

--- utils/service/cron_schedule_message.ts
+++ utils/service/cron_schedule_message.ts
@@ -4,6 +4,8 @@ import {
   GENERAL_QUESTIONS_START_DATE,
 } from "../constants/consts.ts";
 
+import { getOffsetDay } from "../helpers/generate-offset-date.ts";
+
 import { flatten_object } from "../helpers/flatten-object.ts";
 import questions from "../constants/general-questions.json" assert { type: "json" };
 
@@ -14,9 +16,20 @@ import { send_scheduled_message } from "../../src/Events/send-scheduled-message.
 const PREPPED_QUESTIONS = flatten_object(questions);
 
 cron.schedule(CRON_FOR_SCHEDULE_MESSAGE, () => {
-  send_scheduled_message(
-    PREPPED_QUESTIONS,
-    channels.general,
-    GENERAL_QUESTIONS_START_DATE
-  );
+  const start_date = new Date(GENERAL_QUESTIONS_START_DATE);
+  const now = new Date();
+
+  const offset_date = getOffsetDay(start_date, now);
+
+  console.log(offset_date);
+
+  if (offset_date % 119) {
+    send_scheduled_message(
+      PREPPED_QUESTIONS,
+      channels.general,
+      GENERAL_QUESTIONS_START_DATE
+    );
+
+    console.log("this block of code ran");
+  }
 });
