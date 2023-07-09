0eeb977a303f73f40fe341487f38850388216a57
Nonso <nonsootoh@gmail.com>
2023-07-08 23:24:12 -0400

    fix: start date for wonder wednesday

--- utils/constants/consts.ts
+++ utils/constants/consts.ts
@@ -2,7 +2,7 @@ export const AXIOM_DATA_SET = true ? "slack-bot" : "slack-bot-dev";
 
 export const GENERAL_QUESTIONS_START_DATE = "June 22, 2023 14:00:00 UTC";
 export const WONDER_WEDNESDAY_QUESTIONS_START_DATE =
-  "July 26, 2023 14:00:00 UTC";
+  "July 12, 2023 14:00:00 UTC";
 
 export const CRON_FOR_SCHEDULE_MESSAGE = "0 0 * * * *";
 
