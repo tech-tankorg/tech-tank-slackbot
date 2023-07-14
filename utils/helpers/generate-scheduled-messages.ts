af17327c42c454cb4dc0590a23544be3f2e9da13
Nonso <nonsootoh@gmail.com>
2023-07-14 13:42:51 -0400

    fix: generate wonder wednesday messages

--- utils/helpers/generate-scheduled-messages.ts
+++ utils/helpers/generate-scheduled-messages.ts
@@ -1,5 +1,8 @@
 import type { day } from "../types/projectTypes.ts";
-import { generate_thoughtful_thursdays_post } from "../helpers/generate_message.ts";
+import {
+  generate_thoughtful_thursdays_post,
+  generate_wonder_wednesday_post,
+} from "../helpers/generate_message.ts";
 import { determine_next_day_function } from "./determine-next-day-function.ts";
 
 const convert_epoch_date_to_iso_date = (epoch_date: number) => {
@@ -34,7 +37,7 @@ export const generate_scheduled_messages = (
 
     const post_of_the_week =
       repeat_day === "wednesday"
-        ? generate_thoughtful_thursdays_post(message, 3)
+        ? generate_wonder_wednesday_post(message, 3)
         : generate_thoughtful_thursdays_post(message, 1);
 
     new_array.push({
