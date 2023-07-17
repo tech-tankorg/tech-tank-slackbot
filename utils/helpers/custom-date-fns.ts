1a87e87a4e3ed1af60eab5e2b56f88c7c2e13f48
Nonso <nonsootoh@gmail.com>
2023-07-17 10:53:17 -0400

    chore: formats upcoming events

--- utils/helpers/custom-date-fns.ts
+++ utils/helpers/custom-date-fns.ts
@@ -1,3 +1,13 @@
+import { isBefore, isAfter, format } from "date-fns";
+
+export const filter_dates_range = (
+  date: Date,
+  start_month_date: Date,
+  end_month_date: Date
+) => {
+  return isAfter(date, start_month_date) && isBefore(date, end_month_date);
+};
+
 export const getOffsetDay = (start_date: Date, curr_date: Date): number => {
   const offSet = Math.floor(curr_date.valueOf() - start_date.valueOf());
   const toDaysConverter = 24 * 60 * 60 * 1000;
--- utils/helpers/generate-newsletter.ts
+++ utils/helpers/generate-newsletter.ts
@@ -1,5 +1,6 @@
-import { format, startOfMonth } from "date-fns";
+import { format, startOfMonth, endOfMonth } from "date-fns";
 import axios from "axios";
+import { filter_dates_range } from "./custom-date-fns.ts";
 
 const generate_sanity_newsletter = async (date: string) => {
   const encodedURI = encodeURIComponent(
@@ -9,6 +10,32 @@ const generate_sanity_newsletter = async (date: string) => {
   const req = await axios.get(API_REQ);
 
   return req.data.result[0];
+};
+
+const getUpcomingEvents = async () => {
+  const API_REQ = `https://www.googleapis.com/calendar/v3/calendars/${process.env.GOOGLE_CALENDAR_ID}/events?key=${process.env.GOOGLE_CALENDAR_API_KEY}`;
+
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "https://www.googleapis.com/auth/calendar.events",
+    },
+  };
+
+  const result = await axios.get(API_REQ, options);
+
+  const events = result.data.items as any[];
+
+  const start_of_month = startOfMonth(new Date());
+  const end_of_month = endOfMonth(new Date());
+
+  const filtered_upcoming_events = events.filter((event) => {
+    const event_date_start = new Date(event.start.dateTime);
+
+    return filter_dates_range(event_date_start, start_of_month, end_of_month);
+  });
+
+  return filtered_upcoming_events;
 };
 
 const transform_to_block = (section: any[]) => {
@@ -21,16 +48,37 @@ const transform_to_block = (section: any[]) => {
   }));
 };
 
+const transform_to_block_upcoming_events = (section: any[]) => {
+  return section.map((sec: any) => ({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: `${format(new Date(sec.start.dateTime), "MMM do")} - ${format(
+        new Date(sec.start.dateTime),
+        "hh:mm a..aa"
+      )} *${sec.summary}* \n\n${sec.description} \n\n Location/Event Link: ${
+        sec.location
+      }`,
+    },
+  }));
+};
+
 export const generate_newsletter = async () => {
   const start_of_month = startOfMonth(new Date());
   const formatted_date = format(start_of_month, "MMMM yyyy");
 
   const request_format_date = format(start_of_month, "yyyy-MM-dd");
 
-  const response = await generate_sanity_newsletter(request_format_date);
+  const response = await Promise.all([
+    generate_sanity_newsletter(request_format_date),
+    getUpcomingEvents(),
+  ]);
 
-  const transform_block_fyi = transform_to_block(response.letter_fyi);
-  const transform_block_info = transform_to_block(response.letter_info);
+  const transform_block_fyi = transform_to_block(response[0].letter_fyi);
+  const transform_block_info = transform_to_block(response[0].letter_info);
+  const transform_block_upcoming_events = transform_to_block_upcoming_events(
+    response[1]
+  );
 
   return {
     mrkdwn: true,
@@ -75,13 +123,7 @@ export const generate_newsletter = async () => {
           text: ":calendar: |   *UPCOMING EVENTS*  | :calendar: ",
         },
       },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "`12/06/23` *Snow Valley* ",
-        },
-      },
+      ...transform_block_upcoming_events,
       {
         type: "divider",
       },
