ecb9002da5bda795d830a8b9f1de1a5b3063e2d0
Nonso <nonsootoh@gmail.com>
2023-07-21 11:44:59 -0400

    fix: filter for upcoming events in gcal service

--- utils/service/google-calendar.ts
+++ utils/service/google-calendar.ts
@@ -1,4 +1,4 @@
-import { startOfMonth, endOfMonth } from "date-fns";
+import { startOfMonth, endOfMonth, isAfter } from "date-fns";
 import { generic_sort_array } from "../helpers/sort-array.ts";
 import { filter_dates_range } from "../helpers/custom-date-fns.ts";
 import type { google_cal_event } from "../types/projectTypes.ts";
@@ -17,18 +17,22 @@ export const getUpcomingEvents = async () => {
 
   const result = await axios.get(API_REQ, options);
 
-  const events = result.data.items as any[];
+  const events = result.data.items as google_cal_event[];
 
   const start_of_month = startOfMonth(new Date());
   const end_of_month = endOfMonth(new Date());
 
   const filtered_upcoming_events = events.filter((event) => {
     const event_date_start = new Date(event.start.dateTime);
+    const today = new Date();
 
-    return filter_dates_range(event_date_start, start_of_month, end_of_month);
+    return (
+      filter_dates_range(event_date_start, start_of_month, end_of_month) &&
+      isAfter(event_date_start, today)
+    );
   });
 
   generic_sort_array(filtered_upcoming_events);
 
-  return filtered_upcoming_events as google_cal_event[];
+  return filtered_upcoming_events;
 };
