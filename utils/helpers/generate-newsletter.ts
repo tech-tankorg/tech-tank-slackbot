43d57442e0ac27e66e4cd8d6b5085dc84b9f885e
Nonso <nonsootoh@gmail.com>
2023-07-20 21:51:43 -0400

    fix: error state for generate newsletter

--- utils/helpers/generate-newsletter.ts
+++ utils/helpers/generate-newsletter.ts
@@ -43,85 +43,120 @@ export const generate_newsletter = async () => {
 
   const request_format_date = format(start_of_month, "yyyy-MM-dd");
 
-  const response = await Promise.all([
-    generate_sanity_newsletter(request_format_date),
-    getUpcomingEvents(),
-  ]);
+  try {
+    const response = await Promise.all([
+      generate_sanity_newsletter(request_format_date),
+      getUpcomingEvents(),
+    ]);
 
-  const transform_block_fyi = transform_to_block(response[0].letter_fyi);
-  const transform_block_info = transform_to_block(response[0].letter_info);
+    const transform_block_fyi = transform_to_block(response[0].letter_fyi);
+    const transform_block_info = transform_to_block(response[0].letter_info);
 
-  const transform_block_upcoming_events = transform_to_block_upcoming_events(
-    response[1]
-  );
+    const transform_block_upcoming_events = transform_to_block_upcoming_events(
+      response[1]
+    );
 
-  return {
-    mrkdwn: true,
-    text: "say hello",
-    blocks: [
-      {
-        type: "header",
-        text: {
-          type: "plain_text",
-          text: ":newspaper: Nemo's Monthly Newsletter  :newspaper:",
-        },
-      },
-      {
-        type: "context",
-        elements: [
-          {
-            text: formatted_date,
-            type: "mrkdwn",
+    return {
+      mrkdwn: true,
+      text: "say hello",
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: ":newspaper: Nemo's Monthly Newsletter  :newspaper:",
           },
-        ],
-      },
-      {
-        type: "divider",
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: " :loud_sound: *IN CASE YOU MISSED IT* :loud_sound:",
         },
-      },
-
-      ...transform_block_info,
-
-      {
-        type: "divider",
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: ":calendar: |   *UPCOMING EVENTS*  | :calendar: ",
+        {
+          type: "context",
+          elements: [
+            {
+              text: formatted_date,
+              type: "mrkdwn",
+            },
+          ],
         },
-      },
-      ...transform_block_upcoming_events,
-      {
-        type: "divider",
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "*FOR YOUR INFORMATION* :reminder_ribbon:",
+        {
+          type: "divider",
         },
-      },
-      ...transform_block_fyi,
-      {
-        type: "divider",
-      },
-      {
-        type: "context",
-        elements: [
-          {
+        {
+          type: "section",
+          text: {
             type: "mrkdwn",
-            text: ":pushpin: Your monthly newsletter brought to you by *Nemo* from *Tech Tank*.",
+            text: " :loud_sound: *IN CASE YOU MISSED IT* :loud_sound:",
           },
-        ],
-      },
-    ],
-  };
+        },
+
+        ...transform_block_info,
+
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: ":calendar: |   *UPCOMING EVENTS*  | :calendar: ",
+          },
+        },
+        ...transform_block_upcoming_events,
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*FOR YOUR INFORMATION* :reminder_ribbon:",
+          },
+        },
+        ...transform_block_fyi,
+        {
+          type: "divider",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: ":pushpin: Your monthly newsletter brought to you by *Nemo* from *Tech Tank*.",
+            },
+          ],
+        },
+      ],
+    };
+  } catch {
+    return {
+      mrkdwn: true,
+      text: "say hello",
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: ":newspaper: Nemo's Monthly Newsletter  :newspaper:",
+          },
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              text: formatted_date,
+              type: "mrkdwn",
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Sorry! No newsletter this month!*",
+          },
+        },
+      ],
+    };
+  }
 };
