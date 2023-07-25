1f99e268612a03e4a2c72f08f4934e13d8c97d67
Nonso <nonsootoh@gmail.com>
2023-07-25 08:02:52 -0400

    refactor: app home view

--- src/Events/app_home.ts
+++ src/Events/app_home.ts
@@ -4,43 +4,33 @@ import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
 
 import { generate_newsletter } from "../../utils/helpers/generate-newsletter.ts";
 
-import type { app_home_view_response } from "../../utils/types/projectTypes.ts";
 import { is_admin } from "../../utils/helpers/feat-flag.ts";
 
 export const app_home_opened = () => {
   app.event("app_home_opened", async ({ payload, client }) => {
-    let published_view: app_home_view_response;
     const userId = payload.user;
 
+    const view_to_show = is_admin(userId)
+      ? (await generate_newsletter()).blocks
+      : [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: ":construction: Under construction :construction:",
+            },
+          },
+        ];
+
     try {
-      if (is_admin(userId)) {
-        const newsletter = (await generate_newsletter()).blocks;
-        published_view = await client.views.publish({
-          user_id: userId,
-          view: {
-            // Home tabs must be enabled in your app configuration page under "App Home"
-            type: "home",
-            blocks: newsletter,
-          },
-        });
-      } else {
-        published_view = await client.views.publish({
-          user_id: userId,
-          view: {
-            // Home tabs must be enabled in your app configuration page under "App Home"
-            type: "home",
-            blocks: [
-              {
-                type: "header",
-                text: {
-                  type: "plain_text",
-                  text: "Under construction",
-                },
-              },
-            ],
-          },
-        });
-      }
+      const published_view = await client.views.publish({
+        user_id: userId,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          type: "home",
+          blocks: view_to_show,
+        },
+      });
 
       await Axiom.ingestEvents(AXIOM_DATA_SET, [
         { app_home_opened: published_view },
