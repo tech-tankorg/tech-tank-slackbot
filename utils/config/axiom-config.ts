d07dbfa921a7869e367e57117daa451dc073ca39
Nonso <nonsootoh@gmail.com>
2023-08-17 19:04:41 -0400

    chore: updates packages + axiom config

--- utils/config/axiom-config.ts
+++ utils/config/axiom-config.ts
@@ -1,4 +1,7 @@
-import { Axiom } from "@axiomhq/js";
+import pkg from "@axiomhq/js";
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const { Axiom } = pkg;
 
 const client = new Axiom({
   token: process.env.AXIOM_PERSONAL_TOKEN,
