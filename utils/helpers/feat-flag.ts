836693e0ccf82ff5d2e020dada6ea193cbe6072e
Nonso <nonsootoh@gmail.com>
2023-08-21 14:38:04 -0500

    chore: removes test user feat flag

--- utils/helpers/feat-flag.ts
+++ utils/helpers/feat-flag.ts
@@ -3,7 +3,7 @@ import { admins } from "../../utils/config/channel-config.ts";
 export const is_admin = (user_id: string) => {
   const admins_id = Object.values(admins) as string[];
 
-  if (admins_id.includes(user_id) || user_id === "U05BYP96Q6B") return true;
+  if (admins_id.includes(user_id)) return true;
 
   return false;
 };
