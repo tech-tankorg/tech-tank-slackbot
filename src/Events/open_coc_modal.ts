c2227fe5f8f492b59f37017c1fdf8148c599d6fc
Nonso <nonsootoh@gmail.com>
2023-08-22 22:21:41 -0500

    chore: removes unused packages and comments

--- src/Events/open_coc_modal.ts
+++ src/Events/open_coc_modal.ts
@@ -1,8 +1,6 @@
 import app from "../../utils/config/slack-config.ts";
 import { coc_view_modal } from "../../utils/constants/consts.ts";
-// import type { InteractiveMessage } from "@slack/bolt";
-
-// type ff = Pick<SlackAction,"">
+import { channels } from "../../utils/config/channel-config.ts";
 
 export const open_coc_modal = () => {
   app.action("open_coc_modal", async ({ ack, body, client }) => {
@@ -12,6 +10,24 @@ export const open_coc_modal = () => {
     await client.views.open({
       trigger_id: pre_body.trigger_id,
       view: coc_view_modal,
+    });
+  });
+};
+
+export const accept_coc = () => {
+  app.action("accept-coc", async ({ ack }) => {
+    await ack();
+  });
+};
+
+export const deny_coc = () => {
+  app.action("deny-coc", async ({ ack, client, body }) => {
+    await ack();
+    const userID = body.user.id;
+
+    await client.chat.postMessage({
+      channel: channels.notification,
+      text: `Hey <@channel>, <@${userID}> has declined the Tech Tank CoC. Let's connect with them to see how a resolution can be reached.`,
     });
   });
 };
