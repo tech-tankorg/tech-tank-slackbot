8d1f058173ffc5957831863c9e2c79d82dbc2b15
Nonso <nonsootoh@gmail.com>
2023-08-31 16:26:50 -0400

    chore: removes feature flag

--- src/Events/app_home.ts
+++ src/Events/app_home.ts
@@ -4,23 +4,11 @@ import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
 
 import { generate_newsletter } from "../../utils/helpers/generate-newsletter.ts";
 
-import { is_admin } from "../../utils/helpers/feat-flag.ts";
-
 export const app_home_opened = () => {
   app.event("app_home_opened", async ({ payload, client }) => {
     const userId = payload.user;
 
-    const view_to_show = is_admin(userId)
-      ? (await generate_newsletter()).blocks
-      : [
-          {
-            type: "header",
-            text: {
-              type: "plain_text",
-              text: ":construction: Under construction :construction:",
-            },
-          },
-        ];
+    const view_to_show = (await generate_newsletter()).blocks;
 
     try {
       const published_view = await client.views.publish({
