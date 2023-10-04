138d5165f26cc554211a7fa91815b85da377689c
Nonso <nonsootoh@gmail.com>
2023-10-04 14:10:54 -0400

    chore: pauses wed/thurs questions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 // import app from "@utils/config/slack-config.ts";
 import app from "../utils/config/slack-config.ts";
 import client from "../utils/config/axiom-config.ts";
-import { channels } from "../utils/config/channel-config.ts";
 
 import { greet_new_team_member } from "./Events/greetings.ts";
 import { sayHello } from "./Events/sayHello.ts";
-import { thoughtful_thursday_send_scheduled_message } from "./Events/send-scheduled-message.ts";
-import { wonder_wednesday_send_schedule_message } from "./Events/wonder-wednesday-schedule-message.ts";
+
 import { app_home_opened } from "./Events/app_home.ts";
 import {
   open_coc_modal,
@@ -18,37 +16,32 @@ import { thanks } from "../src/Events/thanks.ts";
 import { jokes } from "./Slash-commands/jokes.ts";
 import { suggestion } from "./Slash-commands/suggestions.ts";
 
-import {
-  GENERAL_QUESTIONS_START_DATE,
-  WONDER_WEDNESDAY_QUESTIONS_START_DATE,
-  AXIOM_DATA_SET,
-} from "../utils/constants/consts.ts";
-import { flatten_object } from "../utils/helpers/flatten-object.ts";
-import questions from "../utils/constants/general-questions.json" assert { type: "json" };
-import wonder_wednesday_questions from "../utils/constants/wonder-wednesday-questions.json" assert { type: "json" };
+import { AXIOM_DATA_SET } from "../utils/constants/consts.ts";
 
-const PREPPED_QUESTIONS = flatten_object(questions);
+// const PREPPED_QUESTIONS = flatten_object(questions);
 // const test_channel = "C05BYP98MTR";
 // const test_channel_two = "C05C6KVHWAJ";
 
 // Events
 sayHello();
 greet_new_team_member();
-void thoughtful_thursday_send_scheduled_message(
-  PREPPED_QUESTIONS,
-  channels.general,
-  GENERAL_QUESTIONS_START_DATE,
-  "thursday",
-  1
-);
 
-void wonder_wednesday_send_schedule_message(
-  wonder_wednesday_questions,
-  channels.study,
-  WONDER_WEDNESDAY_QUESTIONS_START_DATE,
-  "wednesday",
-  2
-);
+// Temporally disables the questions
+// void thoughtful_thursday_send_scheduled_message(
+//   PREPPED_QUESTIONS,
+//   channels.general,
+//   GENERAL_QUESTIONS_START_DATE,
+//   "thursday",
+//   1
+// );
+
+// void wonder_wednesday_send_schedule_message(
+//   wonder_wednesday_questions,
+//   channels.study,
+//   WONDER_WEDNESDAY_QUESTIONS_START_DATE,
+//   "wednesday",
+//   2
+// );
 
 app_home_opened();
 
--- utils/service/cron_schedule_message.ts
+++ utils/service/cron_schedule_message.ts
@@ -1,24 +1,21 @@
 import { CronJob } from "cron";
 import {
-  CRON_FOR_SCHEDULE_MESSAGE,
   CRON_FOR_NEWSLETTER,
   CRON_EVERY_MONDAY_AT_10,
   CRON_FOR_MENTOR_CHECKUP,
-  GENERAL_QUESTIONS_START_DATE,
-  WONDER_WEDNESDAY_QUESTIONS_START_DATE,
   TORONTO_TIME_ZONE_IDENTIFIER,
 } from "../constants/consts.ts";
 
-import { getOffsetDay } from "../helpers/custom-date-fns.ts";
+// import { getOffsetDay } from "../helpers/custom-date-fns.ts";
 
-import { flatten_object } from "../helpers/flatten-object.ts";
-import questions from "../constants/general-questions.json" assert { type: "json" };
-import wonder_wednesday_questions from "../constants/wonder-wednesday-questions.json" assert { type: "json" };
+// import { flatten_object } from "../helpers/flatten-object.ts";
+// import questions from "../constants/general-questions.json" assert { type: "json" };
+// import wonder_wednesday_questions from "../constants/wonder-wednesday-questions.json" assert { type: "json" };
 
-import { channels } from "../config/channel-config.ts";
+// import { channels } from "../config/channel-config.ts";
 
-import { thoughtful_thursday_send_scheduled_message } from "../../src/Events/send-scheduled-message.ts";
-import { wonder_wednesday_send_schedule_message } from "../../src/Events/wonder-wednesday-schedule-message.ts";
+// import { thoughtful_thursday_send_scheduled_message } from "../../src/Events/send-scheduled-message.ts";
+// import { wonder_wednesday_send_schedule_message } from "../../src/Events/wonder-wednesday-schedule-message.ts";
 
 import { post_newsletter } from "../../src/Events/post_newsletter.ts";
 
@@ -27,51 +24,51 @@ import { mentor_checkup } from "src/Events/mentor_checkup.ts";
 import { send_weekly_welcome_message } from "../../src/Events/weekly_welcome_message.ts";
 import { post_thanks_message } from "../../src/Events/thanks.ts";
 
-const PREPPED_QUESTIONS = flatten_object(questions);
+// const PREPPED_QUESTIONS = flatten_object(questions);
 
-const job_1 = new CronJob(
-  CRON_FOR_SCHEDULE_MESSAGE,
-  () => {
-    const start_date_thoughtful_thursdays = new Date(
-      GENERAL_QUESTIONS_START_DATE
-    );
-    const start_date_wonder_wednesdays = new Date(
-      WONDER_WEDNESDAY_QUESTIONS_START_DATE
-    );
-    const now = new Date();
+// const job_1 = new CronJob(
+//   CRON_FOR_SCHEDULE_MESSAGE,
+//   () => {
+//     const start_date_thoughtful_thursdays = new Date(
+//       GENERAL_QUESTIONS_START_DATE
+//     );
+//     const start_date_wonder_wednesdays = new Date(
+//       WONDER_WEDNESDAY_QUESTIONS_START_DATE
+//     );
+//     const now = new Date();
 
-    const offset_date_thoughtful_thursdays = getOffsetDay(
-      start_date_thoughtful_thursdays,
-      now
-    );
+//     const offset_date_thoughtful_thursdays = getOffsetDay(
+//       start_date_thoughtful_thursdays,
+//       now
+//     );
 
-    const offset_date_wonder_wednesdays = getOffsetDay(
-      start_date_wonder_wednesdays,
-      now
-    );
+//     const offset_date_wonder_wednesdays = getOffsetDay(
+//       start_date_wonder_wednesdays,
+//       now
+//     );
 
-    if (offset_date_thoughtful_thursdays % 119 === 0)
-      void thoughtful_thursday_send_scheduled_message(
-        PREPPED_QUESTIONS,
-        channels.general,
-        GENERAL_QUESTIONS_START_DATE,
-        "thursday",
-        1
-      );
+//     if (offset_date_thoughtful_thursdays % 119 === 0)
+//       void thoughtful_thursday_send_scheduled_message(
+//         PREPPED_QUESTIONS,
+//         channels.general,
+//         GENERAL_QUESTIONS_START_DATE,
+//         "thursday",
+//         1
+//       );
 
-    if (offset_date_wonder_wednesdays % 119 === 0)
-      void wonder_wednesday_send_schedule_message(
-        wonder_wednesday_questions,
-        channels.study,
-        WONDER_WEDNESDAY_QUESTIONS_START_DATE,
-        "wednesday",
-        2
-      );
-  },
-  null,
-  false,
-  TORONTO_TIME_ZONE_IDENTIFIER
-);
+//     if (offset_date_wonder_wednesdays % 119 === 0)
+//       void wonder_wednesday_send_schedule_message(
+//         wonder_wednesday_questions,
+//         channels.study,
+//         WONDER_WEDNESDAY_QUESTIONS_START_DATE,
+//         "wednesday",
+//         2
+//       );
+//   },
+//   null,
+//   false,
+//   TORONTO_TIME_ZONE_IDENTIFIER
+// );
 
 // Turn on once newsletter is finished
 
@@ -106,9 +103,8 @@ const job_4 = new CronJob(
   TORONTO_TIME_ZONE_IDENTIFIER
 );
 
-job_1.start();
 job_2.start();
 job_3.start();
 job_4.start();
 
-console.log(job_1.running, job_2.running, job_3.running, job_4.running);
+console.log(job_2.running, job_3.running, job_4.running);
