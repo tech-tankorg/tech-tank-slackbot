160ecc3fed9f14bc49929327a46cd3633d7e2a40
Nonso <nonsootoh@gmail.com>
2023-10-20 22:47:56 -0400

    chore: changes zod error

--- utils/types/zod-types.ts
+++ utils/types/zod-types.ts
@@ -36,8 +36,10 @@ export const welcomes_type = z.object({
 });
 
 export const shoutout_message_user_text_validation = z
-  .string()
-  .regex(THANKS_CHANNEL_MESSAGE_VALIDATION_REGEX);
+  .string({ required_error: "" })
+  .regex(THANKS_CHANNEL_MESSAGE_VALIDATION_REGEX, {
+    message: "The message must only contain one keyword",
+  });
 
 export const db_thanks_type = z.object({
   user_id_sender: z.string(),
