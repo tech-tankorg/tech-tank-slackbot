9b9deedc544748d9bdf38a2adda5b2b4f5857037
Nonso <nonsootoh@gmail.com>
2024-03-17 20:03:36 -0400

    chore: adds logger to edit messages commands

--- src/Slash-commands/edit-message.ts
+++ src/Slash-commands/edit-message.ts
@@ -10,6 +10,10 @@ import { subDays } from "date-fns";
 
 import type { ViewOpenResponse } from "../../utils/types/projectTypes.ts";
 
+import Axiom from "../../utils/config/axiom-config.ts";
+import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
+
+let edit_message_view: ViewOpenResponse;
 let handle_message_submit_open_views: ViewOpenResponse;
 let handle_view_message_submit_open_views: ViewOpenResponse;
 
@@ -18,6 +22,7 @@ export const edit_message_command = () => {
     await ack();
     const trigger_id = body.trigger_id;
     const user_id = body.user_id;
+    const user_name = body.user_name;
 
     if (!is_admin(user_id)) {
       await respond({
@@ -28,47 +33,91 @@ export const edit_message_command = () => {
       return;
     }
 
-    await client.views.open({
-      trigger_id,
-      view: edit_message_modal(user_id),
-    });
+    try {
+      edit_message_view = await client.views.open({
+        trigger_id,
+        view: edit_message_modal(user_id),
+      });
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          edit_message_view: {
+            user_id,
+            user_name,
+            status: edit_message_view.ok,
+          },
+        },
+      ]);
+    } catch {
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          error_edit_message_view: {
+            user_id,
+            user_name,
+            status: edit_message_view.ok,
+          },
+        },
+      ]);
+    }
   });
 };
 
 export const handle_edit_message_submit = () => {
   app.view("edit-message-modal", async ({ ack, view, client, body }) => {
     await ack();
-
     // biome-ignore lint: incorrect typescript types therefore "any" is here
     const bdy = body as any;
+    const user_id = body.user.id;
+    const user_name = body.user.name;
 
-    const view_values = view.state.values;
-    const convo_select = view_values["conv_select-block"]
-      ? view_values["conv_select-block"]["select-convo-action"]
-          ?.selected_conversation
-      : undefined;
+    try {
+      const view_values = view.state.values;
+      const convo_select = view_values["conv_select-block"]
+        ? view_values["conv_select-block"]["select-convo-action"]
+            ?.selected_conversation
+        : undefined;
 
-    const user_select = view_values["conv_select-block"]
-      ? view_values["conv_select-block"]["select-user-action"]?.selected_user
-      : undefined;
+      const user_select = view_values["conv_select-block"]
+        ? view_values["conv_select-block"]["select-user-action"]?.selected_user
+        : undefined;
 
-    const today = new Date();
-    const oldestTime = subDays(today, 3);
-    const unixTimestamp = Math.floor(oldestTime.getTime() / 1000);
+      const oldestTime = subDays(new Date(), 3);
+      const unixTimestamp = Math.floor(oldestTime.getTime() / 1000);
 
-    const history_msg = await client.conversations.history({
-      channel: convo_select ?? "",
-      oldest: String(unixTimestamp),
-    });
+      const history_msg = await client.conversations.history({
+        channel: convo_select ?? "",
+        oldest: String(unixTimestamp),
+      });
 
-    const user_msg_history = history_msg.messages?.filter(
-      (msg) => msg.user === user_select
-    );
+      const user_msg_history = history_msg.messages?.filter(
+        (msg) => msg.user === user_select
+      );
 
-    handle_message_submit_open_views = await client.views.open({
-      view: show_message_for_user(user_msg_history ?? [], convo_select ?? ""),
-      trigger_id: bdy.trigger_id,
-    });
+      handle_message_submit_open_views = await client.views.open({
+        view: show_message_for_user(user_msg_history ?? [], convo_select ?? ""),
+        trigger_id: bdy.trigger_id,
+      });
+
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          handle_edit_message_submit: {
+            user_id,
+            user_name,
+            status: handle_message_submit_open_views.ok,
+          },
+        },
+      ]);
+    } catch {
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          error_handle_edit_message_submit: {
+            user_id,
+            user_name,
+            status: handle_message_submit_open_views.ok,
+            error: handle_message_submit_open_views.error,
+          },
+        },
+      ]);
+    }
   });
 };
 
@@ -78,30 +127,54 @@ export const handle_view_message_submit = () => {
 
     // biome-ignore lint: incorrect typescript types therefore "any" is here
     const bdy = body as any;
+    const user_id = body.user.id;
+    const user_name = body.user.name;
 
-    const view_values = view.state.values;
-    const msg_timestamp = view_values["select-timestamp-block"]
-      ? view_values["select-timestamp-block"]["select-timestamp-action"]
-          ?.selected_option?.value
-      : undefined;
+    try {
+      const view_values = view.state.values;
+      const msg_timestamp = view_values["select-timestamp-block"]
+        ? view_values["select-timestamp-block"]["select-timestamp-action"]
+            ?.selected_option?.value
+        : undefined;
 
-    const channel = handle_message_submit_open_views.view?.private_metadata;
+      const channel = handle_message_submit_open_views.view?.private_metadata;
 
-    const history_msg = await client.conversations.history({
-      channel: channel ?? "",
-    });
+      const history_msg = await client.conversations.history({
+        channel: channel ?? "",
+      });
 
-    const user_msg_history = history_msg.messages?.filter(
-      (msg) => msg.ts === msg_timestamp
-    );
+      const user_msg_history = history_msg.messages?.filter(
+        (msg) => msg.ts === msg_timestamp
+      );
 
-    //biome-ignore lint: The below syntax is easier to read
-    const previous_msg = (user_msg_history && user_msg_history[0]?.text) || "";
+      const previous_msg =
+        //biome-ignore lint: The below syntax is easier to read
+        (user_msg_history && user_msg_history[0]?.text) || "";
 
-    handle_view_message_submit_open_views = await client.views.open({
-      view: edit_message_tab(msg_timestamp ?? "", previous_msg),
-      trigger_id: bdy.trigger_id,
-    });
+      handle_view_message_submit_open_views = await client.views.open({
+        view: edit_message_tab(msg_timestamp ?? "", previous_msg),
+        trigger_id: bdy.trigger_id,
+      });
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          handle_view_message_submit: {
+            user_id,
+            user_name,
+            status: handle_view_message_submit_open_views.ok,
+          },
+        },
+      ]);
+    } catch {
+      await Axiom.ingestEvents(AXIOM_DATA_SET, [
+        {
+          error_handle_view_message_submit: {
+            user_id,
+            user_name,
+            status: handle_view_message_submit_open_views.ok,
+          },
+        },
+      ]);
+    }
   });
 };
 
