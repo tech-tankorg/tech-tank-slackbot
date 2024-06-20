1c542bbdf2df1d850d89f31b3f3e2d79687f28b0
Nonso <nonsootoh@gmail.com>
2024-06-20 11:14:49 -0400

    refactor: upload fn

--- src/Slash-commands/slack_upload_files.ts
+++ src/Slash-commands/slack_upload_files.ts
@@ -1,24 +1,16 @@
 import app from "../../utils/config/slack-config.ts";
 import { get_survey_results } from "../../utils/controllers/survey-controller.ts";
-import { writeFile } from "../../lib/node_files/index.ts";
-
 import { randomBytes } from "node:crypto";
 import { survey_results_schema } from "../../utils/types/zod-types.ts";
-
 import { international_timezone_formatter } from "../../utils/helpers/custom-date-fns.ts";
-
 import { is_admin } from "../../utils/helpers/feat-flag.ts";
-
-import path from "node:path";
-
 import { format } from "date-fns";
-
-import { unlinkSync, existsSync } from "node:fs";
-
 import Axiom from "../../utils/config/axiom-config.ts";
 import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
 
 import { channels } from "../../utils/config/channel-config.ts";
+
+import { upload_csv_to_slack } from "../../lib/slack_upload/index.ts";
 
 const generate_values = (value = 16) => {
   return randomBytes(value).toString("hex");
@@ -28,36 +20,10 @@ const sanitize_string = (str: string) => {
   return str.replace(/,/g, "");
 };
 
-const writeCSV = (data: string, file_name: string) => {
-  const cwd = process.cwd();
-  writeFile({ file_name, file_path: cwd, file_type: "csv", data });
-};
-
-const create_file = async (file_name: string) => {
-  const results_pre = await get_survey_results();
-  const results = survey_results_schema.parse(results_pre);
-
-  const columns =
-    "created_at, quarter, question_1, answer_1, question_2, answer_2\n";
-
-  const mapped_value = results.map(
-    (result) =>
-      `${sanitize_string(
-        international_timezone_formatter(result.created_at)
-      )}, ${result.quarter},"${result.response.question_1.q}","${
-        result.response.question_1.a
-      }","${result.response.question_2.q}","${result.response.question_2.a}"\n`
-  );
-  const data_csv = columns + mapped_value.join();
-
-  writeCSV(data_csv, file_name);
-};
-
 export const download_survey_results = () => {
   app.command("/survey-results", async ({ ack, body, client, respond }) => {
     await ack();
     const user_id = body.user_id;
-    let complete_filepath = "";
 
     const is_allowed = is_admin(user_id);
     if (!is_allowed) {
@@ -79,33 +45,45 @@ export const download_survey_results = () => {
 
       return;
     }
-
     try {
+      const file_path = process.cwd();
       const file_name = `temp-${generate_values()}`;
-      await create_file(file_name);
-
-      const cwd = process.cwd();
-      const filename = `${file_name}.csv`;
-      complete_filepath = path.join(cwd, filename);
-
+      const results_pre = await get_survey_results();
+      const results = survey_results_schema.parse(results_pre);
       const today = new Date();
       const formatted_date = format(today, "MM_dd_yyyy");
 
-      const upload = await client.files.uploadV2({
-        file_uploads: [
-          {
-            filename: `survey_results_${formatted_date}.csv`,
-            alt_text: "A file containing the survey results",
-            file: complete_filepath,
-          },
-        ],
-        channel_id: "C05C6KVHWAJ",
-        initial_comment: "Here is a file containing the survey results",
-      });
+      const columns =
+        "created_at, quarter, question_1, answer_1, question_2, answer_2\n";
 
-      if (upload.ok || !upload.ok) {
-        unlinkSync(complete_filepath);
-      }
+      const data = results
+        .map(
+          (result) =>
+            `${sanitize_string(
+              international_timezone_formatter(result.created_at)
+            )}, ${result.quarter},"${result.response.question_1.q}","${
+              result.response.question_1.a
+            }","${result.response.question_2.q}","${
+              result.response.question_2.a
+            }"\n`
+        )
+        .join();
+
+      const file = {
+        columns,
+        csv_data: data,
+        file_name,
+        file_path,
+      };
+
+      const meta_data = {
+        uploaded_file_name: `survey_results_${formatted_date}`,
+        alt_text: "A file containing the survey results",
+        channel_id: channels.admin,
+        comment: "Here is a file containing the survey results",
+      };
+
+      const upload = await upload_csv_to_slack(file, meta_data);
 
       await Axiom.ingestEvents(AXIOM_DATA_SET, [
         {
@@ -113,13 +91,11 @@ export const download_survey_results = () => {
             user_id,
             user_name: body.user_name,
             status: "file uploaded",
-            file: upload.ok,
+            file: upload.status,
           },
         },
       ]);
     } catch (error) {
-      if (existsSync(complete_filepath)) unlinkSync(complete_filepath);
-
       await Axiom.ingestEvents(AXIOM_DATA_SET, [
         {
           error_nemo_survey: {
