41d134e717d5748855ad4f2b2158d133247d31a3
Nonso <nonsootoh@gmail.com>
2024-06-27 09:46:04 -0400

    chore: adds package upgrades

--- src/Events/survey.ts
+++ src/Events/survey.ts
@@ -2,11 +2,14 @@ import { send_message } from "../../utils/helpers/general/send-message.ts";
 import { survey_intro_message } from "../../utils/constants/survey_question_schema.ts";
 import app from "../../utils/config/slack-config.ts";
 
+import Axiom from "../../utils/config/axiom-config.ts";
+import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
+
 export const survey = async () => {
-  const users = await app.client.users.list({});
+  const users = await app.client.users.list();
   const user_ids = users.members?.map((member) => member.id ?? "") ?? [];
 
-  await Promise.all(
+  const sent_messages = await Promise.all(
     user_ids.map((user) =>
       send_message({
         group: "user",
@@ -15,4 +18,16 @@ export const survey = async () => {
       })
     )
   );
+
+  const sent_all = sent_messages.every((message) => message.ok === true);
+
+  await Axiom.ingestEvents(AXIOM_DATA_SET, [
+    {
+      nemo_survey: {
+        number_messaged_users: user_ids.length,
+        all_sent: sent_all,
+        status: "ok",
+      },
+    },
+  ]);
 };
