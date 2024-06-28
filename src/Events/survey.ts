720365cc6d6601095c7bd4719b8f82600221a124
Nonso <nonsootoh@gmail.com>
2024-06-28 10:55:15 -0400

    chore: adds logging to survey sent intros

--- src/Events/survey.ts
+++ src/Events/survey.ts
@@ -5,29 +5,48 @@ import app from "../../utils/config/slack-config.ts";
 import Axiom from "../../utils/config/axiom-config.ts";
 import { AXIOM_DATA_SET } from "../../utils/constants/consts.ts";
 
+import { bots } from "../../utils/config/channel-config.ts";
+
 export const survey = async () => {
-  const users = await app.client.users.list();
-  const user_ids = users.members?.map((member) => member.id ?? "") ?? [];
+  try {
+    const excluded_ids = Object.values(bots);
+    const users = await app.client.users.list();
+    const user_ids =
+      users.members
+        ?.filter(
+          (item) => !excluded_ids.includes(item?.id ?? "") && !item.is_bot
+        )
+        .map((member) => member.id ?? "") ?? [];
 
-  const sent_messages = await Promise.all(
-    user_ids.map((user) =>
-      send_message({
-        group: "user",
-        id: user,
-        input: { type: "blocks", blocks: survey_intro_message(user) },
-      })
-    )
-  );
+    const sent_messages = await Promise.all(
+      user_ids.map((user) =>
+        send_message({
+          group: "user",
+          id: user ?? "",
+          input: { type: "blocks", blocks: survey_intro_message(user) },
+        })
+      )
+    );
 
-  const sent_all = sent_messages.every((message) => message.ok === true);
+    const sent_all = sent_messages.every((message) => message.ok);
 
-  await Axiom.ingestEvents(AXIOM_DATA_SET, [
-    {
-      nemo_survey: {
-        number_messaged_users: user_ids.length,
-        all_sent: sent_all,
-        status: "ok",
+    await Axiom.ingestEvents(AXIOM_DATA_SET, [
+      {
+        nemo_survey: {
+          number_messaged_users: user_ids.length,
+          all_sent: sent_all,
+          status: "ok",
+        },
       },
-    },
-  ]);
+    ]);
+  } catch (error) {
+    await Axiom.ingestEvents(AXIOM_DATA_SET, [
+      {
+        nemo_survey: {
+          all_sent: false,
+          status: "failed to send message",
+        },
+      },
+    ]);
+  }
 };
